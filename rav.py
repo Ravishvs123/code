cb0cc63133346402ece03a6ec6f1fac5ae178331
Ravishvs123 <ravi966304900@gmail.com>
2024-09-05 16:08:43 +0530

    Add files via upload

---
+++ rav.py
@@ -0,0 +1,25 @@
+import mathpoltlid()
+
+q=[]
+def enqueue():
+    if len(q)==size:
+        print("queue is full")
+        else:
+    
+            item=input("enter the element:")
+            q.append(item)
+def dequeue.():
+    if not q:
+        print("queue is empty")
+    else:
+        item=q.pop(0)
+           print("element removed is:",item)
+def display():
+    if n
+    elif choice==2:
+            dequeue()
+    elif choice==3:
+        display()
+    else:
+        break
+import mathpoltlid()
